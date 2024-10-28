b451b2e77f06ac5ff1a3bc0cc792800ade53d1c4
costa-rica <nrodrig1@gmail.com>
2024-10-28 16:24:01 +0100

    ajoutée une icon sur le header

--- screens/NavComposants/Header.js
+++ screens/NavComposants/Header.js
@@ -1,21 +1,23 @@
 import { StyleSheet, Text, View, Image } from 'react-native';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 
-export default function HeaderView(props) {
+export default function HeaderView() {
     console.log('dans HeaderView')
     console.log(`focused: ${props.focused}`)
     console.log(`focused: ${props.screenName}`)
     return (
       <View style={ styles.container }>
-        <View style={props.focused ? styles.sousContainerFocused : styles.sousContainer }>
-        <FontAwesome name={props.iconName} size={props.size} color={props.color} />
+        <View styles={styles.container}>
 
+        <View style={styles.containerLogo}>
         <Image style={styles.image} source={require('../../assets/images/logo128.png')} 
-       alt="logo"
-    //    width={50}
-    //    height={50}
-       resizeMode="contain"
-     />
+            alt="logo" resizeMode="contain"/>
+        </View>
+        
+        <View style={styles.containerProfil}>
+        <FontAwesome name='user' size={25} color={'white'} />
+        </View>
+
         </View>
       </View>
     );
@@ -32,6 +34,9 @@ export default function HeaderView(props) {
         width:'100%',
         borderBottomLeftRadius: 10,    // Radius for top-left corner
         borderBottomRightRadius: 10,   // Radius for top-right corner
+    },
+    containerLogo:{
+      width: '70%'
     },
     image:{
         //width: 30, // Set width to 50% of its original size
